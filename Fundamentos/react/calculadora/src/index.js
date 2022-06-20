3ed2c502b3336f8aa25c078fa43c8c3bc0177d8f
Agnaldo Silva <Agnaldo_ss@hotmail.com>
2022-06-19 22:58:50 -0300

    update-calculadora

--- Fundamentos/react/calculadora/src/index.js
+++ Fundamentos/react/calculadora/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './main/Calculator';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>Calculadora</h1>
-    <Calculator />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <div>
+        <h1>Calculadora</h1>
+        <Calculator />
+    </div>
+, document.getElementById('root'));
+registerServiceWorker();
